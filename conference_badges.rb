4d1800d2b970a232bd872c4d65788e7f278d848f
Preston Davis <prestonseandavis101@yahoo.com>
2020-08-26 12:52:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.each do |attendee|
+    puts "Hello, "
+end
