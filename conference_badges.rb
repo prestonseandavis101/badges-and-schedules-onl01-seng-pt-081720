bfc88ddae826113574261d6568c5fdf082b60ac9
Preston Davis <prestonseandavis101@yahoo.com>
2020-08-26 15:18:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ def assign_rooms(attendees)
   room_assignments = 0
   attendees.collect do |attendee|
     room_assignments +=1
-    "Hello #{attendee}! You'll be assigned to room #{room_assignments}!"
+    "Hello, #{attendee}! You'll be assigned to room #{room_assignments}!"
     
 
 end
